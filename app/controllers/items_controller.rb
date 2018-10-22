2c8b6fc925adca09397ad2eb2739654e4d8acdd8
martin5696 <linxuan.zhang@mail.utoronto.ca>
2018-10-22 12:54:15 -0400

    added controller and model code for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,28 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params.permit(:name ,:category, :quantity, :data_posted, :owner_id, :status))
+    @item.save
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(params)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
 	belongs_to :user
 	has_many :reviews
 	belongs_to :transaction 
+	validates :name ,:category, :quantity, :data_posted, :owner_id, :status, presence: true
 end
