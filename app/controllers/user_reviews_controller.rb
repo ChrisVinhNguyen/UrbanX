876039f748733a8767b423689e76514a94093d66
martin5696 <linxuan.zhang@mail.utoronto.ca>
2018-10-30 14:14:33 -0400

    user review controller

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -1,2 +1,28 @@
 class UserReviewsController < ApplicationController
+  def new
+    @user_review = UserReview.new
+  end
+
+  def create
+    @user_review = UserReview.new(params.permit(:user:id, :rating))
+    @user_review.save
+  end
+
+  def update
+    @user_review = UserReview.find(params[:id])
+    @user_review.update(params)
+  end
+
+  def destroy
+    @user_review = UserReview.find(params[:id])
+    @user_review.destroy
+  end
+
+  def index
+    @user_reviews = UserReview.all
+  end
+  
+  def show
+    @user_review = UserReview.find(params[:id])
+  end
 end
