9b2e01283784c8af28e6ceb93fae71e84a7cd367
martin5696 <linxuan.zhang@mail.utoronto.ca>
2018-10-30 14:12:58 -0400

    more controller code

---
+++ app/controllers/item_reviews_controller.rb
@@ -0,0 +1,28 @@
+class ItemReviewsController < ApplicationController
+  def new
+    @item_review = ItemReview.new
+  end
+
+  def create
+    @item_review = ItemReview.new(params.permit(:user:id, :rating))
+    @item_review.save
+  end
+
+  def update
+    @item_review = ItemReview.find(params[:id])
+    @item_review.update(params)
+  end
+
+  def destroy
+    @item_review = ItemReview.find(params[:id])
+    @item_review.destroy
+  end
+
+  def index
+    @item_reviews = ItemReview.all
+  end
+  
+  def show
+    @item_review = ItemReview.find(params[:id])
+  end
+end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,28 @@
 class TransactionsController < ApplicationController
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(params.permit(:item_id ,:borrower_id, :lender_id, :status))
+    @transaction.save
+  end
+
+  def update
+    @transaction = Transaction.find(params[:id])
+    @transaction.update(params)
+  end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+  end
+
+  def index
+    @transactions = Transaction.all
+  end
+  
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
 end
