50a4795dfed40bda825d278f2790e052b8d60785
Kuan Lin Chen <victorr.chen@mail.utoronto.ca>
2018-10-31 18:01:42 -0400

    generated views and routes for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'transactions/new'
+  get 'transactions/create'
+  get 'transactions/update'
+  get 'transactions/destroy'
+  get 'transactions/index'
+  get 'transactions/show'
   devise_for :users
   root to: "user_profiles#index"
 
   resources :user_profiles
+  resources :transactions
 end
