0db779cda5b603f774f0b8693867d1eb43affb8d
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-11-06 22:44:31 -0500

    added validation inside item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
-	belongs_to :user, class_name: :User, foreign_key: :user_id
-	has_many :item_reviews, dependent: :destroy 
-	has_many :transactions
+  validates :name, :category, :quantity, :condition, presence: true
+  validates :quantity, numericality: { only_integer: true }
+
+  belongs_to :user, class_name: :User, foreign_key: :user_id
+  has_many :item_reviews, dependent: :destroy
+  has_many :transactions
 end
