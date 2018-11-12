0a1d93f1aa7cfd68aac454bcc62bf9a9616442c6
Johnson <johnson.huang@mail.utoronto.ca>
2018-11-11 23:14:30 -0500

    fixed all CRUD actions for user reviews

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -1,12 +1,12 @@
 class UserReviewsController < ApplicationController
   def create
     if user_signed_in?
-      @user_profile = UserProfile.find(params[:user_review][:reviewer_id])
+      @user_profile = UserProfile.find_by user_id: (params[:user_review][:reviewee_id])
 
       user_review_params = permit_user_review_params
       user_review_params[:rating] = user_review_params[:rating]
       user_review_params[:reviewer_id] = @current_user.id
-      user_review_params[:reviewee_id] = @user_profile.id
+      user_review_params[:reviewee_id] = @user_profile.user_id
       user_review_params[:created_at] = DateTime.now
       user_review_params[:updated_at] = DateTime.now
 
@@ -23,55 +23,43 @@ class UserReviewsController < ApplicationController
   end
 
   def new
-    @reviewer_id = params[:reviewer_id]
+    @reviewee_id = params[:reviewee_id]
     @user_review = UserReview.new
   end
 
   def index
-    if user_signed_in?
-      puts(params)
-      @user_profile = UserProfile.find(params[:user_profile_id])
-      @user_reviews = UserReview.where(:reviewee_id => @user_profile.user_id)
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   def show
-    if user_signed_in?
-      @user_profile = UserProfile.find(params[:user_profile_id])
-      @user_reviews = UserReview.where(:reviewer_id => @user_profile.user_id)
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   def update
     if user_signed_in?
-      @user_profile = UserProfile.find(params[:user_profile_id])
-      @user_review = @user_profile.user_reviews.find(params[:id])
+      @user_review = UserReview.find(params[:id])
+      @user_profile = UserProfile.find_by user_id: @user_review.reviewee_id
       #check if current user is allowed to edit
       if @user_review.reviewer_id == @current_user.id
-        if @user_review.update(user_review_params)
+        if @user_review.update(permit_user_review_params)
           puts("saved")
           @user_review.updated_at = DateTime.now
           redirect_to @user_profile
         else
           puts("nots saved")
-          render 'edit'
+          redirect_to @user_profile
         end
       else
-        render 'edit'
+          puts(@user_review.comment)
+          puts(@current_user.id)
       end
     else
-      redirect_to new_user_session_path
+      redirect_to @user_profile
     end
   end
 
   def edit
     if user_signed_in?
-      @user_profile = UserProfile.find(params[:user_profile_id])
-      @user_review = @user_profile.user_reviews.find(params[:id])
+      @reviewee_id = params[:reviewee_id]
+      @user_review = UserReview.find(params[:id])
     else 
       redirect_to new_user_session_path
     end
@@ -79,8 +67,8 @@ class UserReviewsController < ApplicationController
 
   def destroy
     if user_signed_in?
-      @user_profile = UserProfile.find(params[:user_profile_id])
-      @user_review = @user_profile.user_reviews.find(params[:id])
+      @user_review = UserReview.find(params[:id])
+      @user_profile = UserProfile.find_by user_id: @user_review.reviewee_id
       @user_review.destroy
       redirect_to @user_profile
     else
