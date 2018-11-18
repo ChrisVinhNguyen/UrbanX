8da5a0b7400cae7eef76549c0cf3148a66a6e0d5
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-11-18 15:26:31 -0500

    fix minor syntax

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Header />
         <Body />
         <Footer />
--- app/javascript/packs/components/Body.jsx
+++ app/javascript/packs/components/Body.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Body extends React.Component {
+class Body extends Component {
   render() {
     return (
       <div className="body">
