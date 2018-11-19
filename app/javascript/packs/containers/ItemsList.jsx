12cfb4e240b89414206c8ed27cddc20b8f08d225
martin5696 <linxuan.zhang@mail.utoronto.ca>
2018-11-19 13:26:22 -0500

    Updated Item

--- app/javascript/packs/containers/ItemsList.jsx
+++ app/javascript/packs/containers/ItemsList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Grid } from 'semantic-ui-react'
 
 import Item from '../components/Item'
 
@@ -27,7 +28,39 @@ class ItemsList extends Component {
     let items = this.state.items.map(item => {return <Item key={item.id} item={item}/>;});
     return (
       <div className="body">
-        {items}
+        <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+        {items[0]}
+      </Grid.Column>
+      <Grid.Column>
+        {items[1]}
+      </Grid.Column>
+      <Grid.Column>
+        {items[2]}
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        {items[3]}
+      </Grid.Column>
+      <Grid.Column>
+        {items[4]}
+      </Grid.Column>
+      <Grid.Column>
+        {items[5]}
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        {items[6]}
+      </Grid.Column>
+
+    </Grid.Row>
+  </Grid>
+
       </div>
     );
   }
