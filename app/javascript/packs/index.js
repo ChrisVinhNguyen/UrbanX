a3571195b96893fcc7f0ef164b592c465fd802b6
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-11-20 19:49:14 -0500

    adding a single scss file that the application references

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
+import '../styles/index.scss'
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <BrowserRouter>
