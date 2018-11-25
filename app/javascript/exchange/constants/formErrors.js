14893a96d4112aed049881ad2f12a30467a5f3ed
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-11-25 02:41:48 -0500

    adding validation to sign in form if fields are missing

--- app/javascript/exchange/constants/formErrors.js
+++ app/javascript/exchange/constants/formErrors.js
@@ -3,5 +3,7 @@ export const PASSWORD_MISSING = 'Your password is missing';
 export const CONFIRMATION_PASSWORD_MISSING = 'Your confirmation password is missing';
 export const INVALID_EMAIL = 'This is an invalid email';
 export const EMAIL_ALREADY_IN_USE = 'This email is already in use';
+export const EMAIL_DOES_NOT_EXIST = 'This email does not exist';
+export const INCORRECT_PASSWORD = 'This is the wrong password';
 export const INVALID_PASSWORD_LENGTH = 'The password must be 6 to 30 characters in length';
 export const PASSWORD_MISMATCH = 'Your passwords do not match';
--- app/javascript/exchange/containers/UserSignInFormContainer.jsx
+++ app/javascript/exchange/containers/UserSignInFormContainer.jsx
@@ -1,8 +1,13 @@
-import React, { Component } from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { v4 as uuid } from 'uuid';
 import axios from 'axios';
+import { Button, Checkbox, Form, Message } from 'semantic-ui-react';
+
 import { signInUser } from '../actions/userActions';
 import { connect } from 'react-redux';
+
+import { EMAIL_MISSING, PASSWORD_MISSING } from '../constants/formErrors';
+
 
 class UserSignInFormContainer extends Component {
   constructor() {
@@ -12,11 +17,13 @@ class UserSignInFormContainer extends Component {
       password: '',
       emailError: false,
       passwordError: false,
-      errorMessages: []
+      errorMessages: [],
+      formError: false
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFormErrors = this.handleFormErrors.bind(this);
   }
 
   handleChange(e, { name, value }) {
@@ -24,26 +31,77 @@ class UserSignInFormContainer extends Component {
   }
 
   handleSubmit(e) {
+    const errorMessages = this.handleFormErrors();
+
+    if (errorMessages.length > 0) {
+      this.setState({
+        formError: true,
+        errorMessages: errorMessages
+      })
+      return
+    } else {
+      this.setState({ formError: false })
+    }
+
     let userData = this.state;
 
     this.props.signInUser(userData);
   }
 
+  handleFormErrors() {
+    let errorMessages = [];
+
+    if (this.state.email === '') {
+      this.setState({ emailError: true })
+      errorMessages.push(EMAIL_MISSING);
+    } else {
+      this.setState({ emailError: false })
+    }
+    if (this.state.password === '') {
+      this.setState({ passwordError: true })
+      errorMessages.push(PASSWORD_MISSING);
+    } else {
+      this.setState({ passwordError: false })
+    }
+
+    return errorMessages;
+  }
+
   render() {
-    const { email, password } = this.state
+    const { email, password, emailError, passwordError, errorMessages, formError } = this.state
+
+    const errorMessageContent = this.state.errorMessages.map(message => {
+      const keyVal = uuid();
+      return (
+        <li key={ keyVal }>{message}</li>
+      )
+    })
+
+    const errorMessage = (
+      <Message
+        error
+        header='Form Error(s)'
+        content={
+          <ul>
+            {errorMessageContent}
+          </ul>
+        }
+      />
+    )
 
     return (
       <div className="sign-in-form-container">
         <h1 className="sign-in-form-title">Sign In Form</h1>
-        <Form className="sign-in-form" onSubmit={ this.handleSubmit }>
+        <Form className="sign-in-form" onSubmit={ this.handleSubmit } error={ formError }>
           <Form.Field>
             <label>Email</label>
-            <Form.Input placeholder='bob.smith@gmail.com' name='email' value={ email } onChange={ this.handleChange }  width={10} />
+            <Form.Input placeholder='bob.smith@gmail.com' name='email' value={ email } onChange={ this.handleChange }  width={10} error={ emailError } />
           </Form.Field>
           <Form.Field>
             <label>Password</label>
-            <Form.Input type='password' name='password' value={ password } onChange={ this.handleChange }  width={10} />
+            <Form.Input type='password' name='password' value={ password } onChange={ this.handleChange }  width={10} error={ passwordError } />
           </Form.Field>
+          { errorMessages.length > 0 ? errorMessage : null }
           <Form.Button content='Submit' />
         </Form>
       </div>
