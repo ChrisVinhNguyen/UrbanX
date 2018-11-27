16b8721d0943d3b97c3a06ac5931226f2b8e5722
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-11-27 14:26:04 -0500

    adding partial fix for navigating to other profiles while in another profile

--- app/javascript/exchange/components/UserProfile.jsx
+++ app/javascript/exchange/components/UserProfile.jsx
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import UserProfileContainer from '../containers/UserProfileContainer'
-import '../stylesheets/user-profile.scss'
+import UserProfileContainer from '../containers/UserProfileContainer';
+
+import '../stylesheets/user-profile.scss';
 
 class UserProfile extends Component {
   render() {
-  	console.log(this.props)
-  	var viewingMyProfile  = false
-  	if ( this.props.match.params.id == this.props.currentUserId )
-  	{
-  		viewingMyProfile = true;
-  	}
-    console.log(this.props.match.params.id)
+    let viewingMyProfile = false;
+    if (this.props.match.params.id == this.props.currentUserId) {
+      viewingMyProfile = true;
+    }
+
     return (
       <div style={{marginBottom: '40px', marginTop: '40px'}} className="user-profile">
-        <UserProfileContainer viewingMyProfile={viewingMyProfile} userProfileId = {this.props.match.params.id} /> 
+        <UserProfileContainer viewingMyProfile={ viewingMyProfile } userProfileId={ this.props.match.params.id } />
       </div>
     );
   }
