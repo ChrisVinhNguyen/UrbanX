d502b33065e89c6f97d8e14cfb9f94feaa87a10b
martin5696 <linxuan.zhang@mail.utoronto.ca>
2018-11-27 19:31:54 -0500

    Auto-fille form with existing information when editing user profile

--- app/controllers/authenticate_user_controller.rb
+++ app/controllers/authenticate_user_controller.rb
@@ -7,6 +7,8 @@ class AuthenticateUserController < ApplicationController
         "email" => current_user.email,
         "user_profile_id" => user_profile.id,
         "full_name" => user_profile.full_name,
+        "first_name" => user_profile.first_name,
+        "last_name" => user_profile.last_name,
         "picture" => user_profile.picture,
         "date_of_birth" => user_profile.date_of_birth,
         "location" => user_profile.location,
