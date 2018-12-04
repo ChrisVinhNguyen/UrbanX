a61083475265c53d959b9ed9fde20b422ab4878b
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-12-03 23:27:16 -0500

    moving item detail content to right side of image

--- app/javascript/exchange/components/ItemDetailsComponent.jsx
+++ app/javascript/exchange/components/ItemDetailsComponent.jsx
@@ -37,28 +37,30 @@ class ItemDetailsComponent extends Component {
 
     return (
       <div>
-        <Item>
-          <Item.Content>
-          { this.props.item_images_prop && this.props.item_images_prop.length > 0 ?
-            imageGallery :
-            <Image size='medium' src={pic} />
-          }
-          <Item.Header size = 'medium' as='a' ><strong>{this.props.item_name_prop}</strong></Item.Header>
-        <Item.Meta>
-          <p>Quantity: {this.props.item_quantity_prop}</p>
-          
-          <ItemDetailsBorrowContainer item_id={ this.props.item_match_param_id_prop} currentUserId={ this.props.current_user_id_prop } userProfileId={ this.props.item_owner_profile_id }/>
-        </Item.Meta>
-        <Item.Description>Description: {this.props.item_description_prop}.</Item.Description>
-        {this.props.active_transactions_msg_prop}
-        {this.props.edit_button_prop}
-        {this.props.delete_button_prop}
-        <Item.Extra>
-          <Rating icon='star' rating={this.props.item_avg_rating_prop} maxRating={5} disabled />
-          <Label>Rating: {this.props.item_avg_rating_prop}</Label>
-        </Item.Extra>
-      </Item.Content>
-        </Item>
+        <Item.Group>
+          <Item>
+            { this.props.item_images_prop && this.props.item_images_prop.length > 0 ?
+              imageGallery :
+              <Image size='medium' src={pic} />
+            }
+            <Item.Content>
+              <Item.Header size = 'medium' as='a' ><strong>{this.props.item_name_prop}</strong></Item.Header>
+              <Item.Meta>
+                <p>Quantity: {this.props.item_quantity_prop}</p>
+                
+                <ItemDetailsBorrowContainer item_id={ this.props.item_match_param_id_prop} currentUserId={ this.props.current_user_id_prop } userProfileId={ this.props.item_owner_profile_id }/>
+              </Item.Meta>
+              <Item.Description>Description: {this.props.item_description_prop}.</Item.Description>
+              {this.props.active_transactions_msg_prop}
+              {this.props.edit_button_prop}
+              {this.props.delete_button_prop}
+              <Item.Extra>
+                <Rating icon='star' rating={this.props.item_avg_rating_prop} maxRating={5} disabled />
+                <Label>Rating: {this.props.item_avg_rating_prop}</Label>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
         <ItemReviewsContainer current_viewed_item_id={this.props.item_match_param_id_prop} item_owner_profile_id={this.props.item_owner_profile_id} item_owner_user_id={this.props.item_details_user_id_prop}/>
       </div>
     );
